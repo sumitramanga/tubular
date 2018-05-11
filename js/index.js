d07ed8f0894448adc961b98ab425543321edd2f3
Sumitra Manga <sumitramanga@Parbhus-MacBook-Air.local>
2018-05-11 16:41:15 +1200

    Created tubular page

---
+++ js/index.js
@@ -0,0 +1,8 @@
+$('document').ready(function() {
+ console.log('alive');
+ var options = { videoId: '6YcofccdkBc', start: 6 };
+ $('#wrapper').tubular(options);
+ // videoId is where the video reference is inserted.
+ // the videoId can be found in the website URL of the selected Youtube video
+
+});
